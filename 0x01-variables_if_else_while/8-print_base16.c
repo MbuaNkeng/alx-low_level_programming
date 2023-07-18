c8be00d978086f7bfa909c07c53e8438b5be038a
MbuaNkeng <nkeng.beza@yahoo.com>
2023-07-18 21:49:06 +0100

    Exerxise 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int n1;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n1 = 97; n1 <= 102; n1++)
+	{
+		putchar(n1);
+	}
+	putchar('\n');
+	return (0);
+}
