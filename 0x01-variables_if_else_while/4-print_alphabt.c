56ededa82280e62a8fe55f0120a4c80059b8888d
MbuaNkeng <nkeng.beza@yahoo.com>
2023-07-18 14:50:36 +0100

    Exercise 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints alphabets in lower and CAPITAL case
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n < 123)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
