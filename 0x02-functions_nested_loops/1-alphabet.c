556de82e0cf6f6c509c699836ea1d03e9a5836d3
MbuaNkeng <nkeng.beza@yahoo.com>
2023-07-18 17:20:02 -0700

    Task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Entry point
- *
+ * Description: function to prit letters of the alphabet in small letters
  * Return: Aways 0 (Success)
  */
 void print_alphabet(void)
@@ -13,5 +13,4 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
