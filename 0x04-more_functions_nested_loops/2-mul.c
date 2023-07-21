aadf93b0969bd3113260252a915e7d2b5b1ad410
MbuaNkeng <nkeng.beza@yahoo.com>
2023-07-21 16:45:15 -0700

    Solution to exersise 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * int mul(int a, int b) - checks product of 2 numbers.
+ * mul - multiply 2 int.
  * @a: The number to be checked.
  * @b: The number to be checked.
  * Return: result for multiplying a and b.
  */
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
